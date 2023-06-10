4c26edbc4b8da5652396ac70ba4ef5e9d9224a5e
AlexNykoliak <nykolyak.o@gmail.com>
2023-06-10 11:59:28 +0300

    Add second lesson for students, Python Basic

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# PythonForStudents_Lesson2
+# Lesson 2 for Python Students
+
+This lesson covers a wide range of basic and intermediate topics in Python, including but not limited to variable types, operators, functions, strings, and lists. Here's the code for this lesson:
+
+```python
+import string
+import random
+
+# Basic Printing
+print("Hello World")
+print(10+10)  # Addition
+print(10-10)  # Subtraction
+print(10*10)  # Multiplication
+print(10**10)  # Exponentiation
+print(10/10)  # Division
+print(10//10)  # Floor Division
+print(10 % 10)  # Modulus
+print("Hello World" + str(100))
+print("Hello World" + "Hello World")  # Concatenation
+print(100, 'Hello World')
+
+# Variable types
+a = 100     # Integer
+b = "Hello World"  # String
+c = 100.1   # Float
+d = True    # Boolean
+e = False   # Boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# Comparisons
+print(10 == 10)  # Equal
+print(10 != 10)  # Not Equal
+print(10 > 10)  # Greater Than
+print(10 < 10)  # Less Than
+print(10 >= 10)  # Greater Than or Equal
+print(10 <= 10)  # Less Than or Equal
+
+# Basic functions
+def function():
+    return 100
+
+def function1():
+    print(100)
+
+print(function())
+print('----------------------')
+print(function1())
+
+# String operations
+my_string = "HelloWorld"
+name = "denys"
+name_upper = name.upper()
+lastname = "KOVALENKO"
+lastname_lower = lastname.lower()
+g = " something "
+g = g.strip()
+f = "something"
+f = f.replace("o", "O")
+price = '100$'
+country = 'Ukraine'
+quantity = 10
+
+print("Price of this item is {} and it was made in {}. We have {} items at our shop.".format(
+    price, country, quantity))
+
+# List operations
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, True, False, [1, 2, 3, 4, 5,
+                                                    6, 7, 8, 9, [1, 2, 3, 4, 5, ['Hi']]], 100.10, 'Hello World']
+
+print(my_list)
+print(type(my_list))    # <class 'list'>
+print(my_list[-1])   # Hello World
+
+print(my_list[11])  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(my_list[11][-1])    # 5
+print(my_list[11][-1][-1][0])    # Hi
+```
+
+Each block represents a different concept in Python, and the comments should help guide you through what each block does.
+
+Remember to practice these examples and modify them to understand how they work. Happy learning!
---
+++ main.py
@@ -0,0 +1,169 @@
+import string
+import random
+
+print("Hello World")
+print(10+10)  # Addition
+print(10-10)    # Subtraction
+print(10*10)    # Multiplication
+print(10**10)   # Exponentiation
+print(10/10)    # Division
+print(10//10)   # Floor Division
+print(10 % 10)    # Modulus
+print("Hello World" + str(100))
+print("Hello World" + "Hello World")    # Concatenation
+print(100, 'Hello World')
+
+a = 100     # Integer
+b = "Hello World"  # String
+c = 100.1   # Float
+d = True    # Boolean
+e = False   # Boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(10 == 10)  # Equal
+print(10 != 10)  # Not Equal
+print(10 > 10)  # Greater Than
+print(10 < 10)  # Less Than
+print(10 >= 10)  # Greater Than or Equal
+print(10 <= 10)  # Less Than or Equal
+
+
+""" This is a function which prints Hello World """
+
+a = 1
+b = ' '
+
+print(id(a))
+print(id(' '))
+print(id(b))
+
+
+numbers = string.digits
+punctuation = string.punctuation
+lower_case = string.ascii_lowercase
+upper_case = string.ascii_uppercase
+all_letters = string.ascii_letters
+
+print(numbers)
+print(punctuation)
+print(lower_case)
+print(upper_case)
+print(all_letters)
+
+
+symbols_for_password = numbers + punctuation + lower_case + upper_case
+
+password = "".join(random.sample(symbols_for_password, 12))
+
+print(password)
+
+
+int()  # Integer
+float()  # Float
+str()  # String
+bool()  # Boolean
+
+a = 100    # Integer
+print(type(a))  # <class 'int'>
+print(a)    # 100
+
+a = float(a)    # 100.0
+a = str(a)  # '100.0'
+print(type(a))  # <class 'str'>
+print(a)   # '100.0'
+print(int(a) + 100)  # 200
+
+
+a, b, c = 100, 200, 300
+
+
+print(a)    # 100
+print(b)    # 200
+print(c)    # 300
+
+
+*z, x, y = 100, 200, 300, 400, 500, 600
+
+print(z)
+print(x)
+print(y)
+
+
+a = b = c = f = 100
+
+b = 200
+
+print(a)
+print(b)
+
+
+def function():
+    return 100
+
+
+def function1():
+    print(100)
+
+
+print(function())
+print('----------------------')
+print(function1())
+
+
+my_string = "HelloWorld"
+print(my_string[0])     # H
+print(my_string[1])     # e
+print(my_string[-1])    # d
+print(my_string[2:6])   # lloW
+print(my_string[2:])    # lloWorld
+print(my_string[:6])    # HelloW
+print(my_string[:])     # HelloWorld
+print(my_string[::2])   # Hlool
+print(my_string[::-1])  # dlroWolleH
+
+
+name = "denys"
+name_upper = name.upper()
+print(name_upper)  # DENYS
+
+lastname = "KOVALENKO"
+lastname_lower = lastname.lower()
+print(lastname_lower)  # kovalenko
+
+
+g = " something "
+print(g)
+g = g.strip()
+print(g)   # something
+
+
+f = "something"
+f = f.replace("o", "O")
+print(f)    # sOmething
+
+
+price = '100$'
+country = 'Ukraine'
+quantity = 10
+
+
+print("Price of this item is {} and it was made in {}. We have {} items at our shop.".format(
+    price, country, quantity))
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, True, False, [1, 2, 3, 4, 5,
+                                                    6, 7, 8, 9, [1, 2, 3, 4, 5, ['Hi']]], 100.10, 'Hello World']
+
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, True, False, [1, 2, 3, 4, 5, 6, 7, 8, 9], 100.1, 'Hello World']
+print(my_list)
+print(type(my_list))    # <class 'list'>
+print(my_list[-1])   # Hello World
+
+print(my_list[11])  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(my_list[11][-1])    # 5
+print(my_list[11][-1][-1][0])    # Hi
