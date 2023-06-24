67240d5fcf313120c5beaf7364b4dd7bdf5887e0
AlexNykoliak <nykolyak.o@gmail.com>
2023-06-24 12:24:34 +0300

    Update readme

---
+++ Lesson4.Basic/README.md
@@ -0,0 +1,56 @@
+#  Python Tuple Tasks
+
+Below are the 25 advanced tasks involving Python tuples for you to work on. 
+
+**Note**: Please make sure to create a new Python file for each task to keep your work organized.
+
+1. **Task 1:** Given a tuple `a = (1, 2, 3, 4, 5)`, write a Python function to convert the tuple into a string.
+
+2. **Task 2:** Write a Python program to check if all items in a tuple are the same.
+
+3. **Task 3:** Write a Python program to replace the last value of a tuple in a list of tuples. List = [(10, 20, 40), (40, 50, 60), (70, 80, 90)].
+
+4. **Task 4:** Write a Python function to flatten a nested tuple.
+
+5. **Task 5:** Write a Python function that takes a tuple and returns a new tuple with all the unique elements.
+
+6. **Task 6:** Write a Python function to check whether an element exists within a tuple.
+
+7. **Task 7:** Write a Python program to slice a tuple.
+
+8. **Task 8:** Write a Python function that accepts a tuple of tuples and returns the tuple with the maximum sum of elements.
+
+9. **Task 9:** Write a Python function that will accept a tuple and return a sorted tuple.
+
+10. **Task 10:** Write a Python function to reverse a tuple.
+
+11. **Task 11:** Write a Python function to compare two tuples element by element and print "Equal" if they are the same, and "Not Equal" if they are different.
+
+12. **Task 12:** Write a Python function to calculate the average of all numbers in a tuple.
+
+13. **Task 13:** Write a Python function to remove an empty tuple(s) from a list of tuples. List = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
+
+14. **Task 14:** Write a Python function that takes a tuple of numbers and returns a tuple of only the numbers divisible by 5.
+
+15. **Task 15:** Write a Python function that will accept a tuple and convert it into a dictionary.
+
+16. **Task 16:** Write a Python function to concatenate tuples to form a new tuple.
+
+17. **Task 17:** Write a Python function that will take a tuple and print the type of each item in the tuple.
+
+18. **Task 18:** Write a Python function to find the repeated items in a tuple.
+
+19. **Task 19:** Write a Python function to find the length of a tuple.
+
+20. **Task 20:** Write a Python function that takes two tuples and returns True if they contain at least one common element.
+
+21. **Task 21:** Write a Python function to convert a tuple to a dictionary. 
+
+22. **Task 22:** Write a Python function to print a tuple with string formatting.
+
+23. **Task 23:** Write a Python program to convert a list to a tuple.
+
+24. **Task 24:** Write a Python function to compute the similarity between two tuples.
+
+25. **Task 25:** Write a Python function to generate a fixed-length tuple filled with a specified character.
+
--- Lesson4.Basic/main.py
+++ Lesson4.Basic/main.py
@@ -1,153 +1,167 @@
-from time import time
-a = [1, 2, 3]
+# from time import time
+# a = [1, 2, 3]
 
-a.append(4)
-print(a)
+# a.append(4)
+# print(a)
 
-a.insert(0, 0)
-print(a)
+# a.insert(0, 0)
+# print(a)
 
-a.remove(3)
+# a.remove(3)
 
-o = a.pop(0)
-print(o)
-
-
-# list, ordered, changeable(mutable), duplicates
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9]
-
-# tuple   ordered, unchangeable(immutable), duplicates
-my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 11)
-
-if 11 in my_tuple:
-    print("Yes")
-else:
-    print("No")
+# o = a.pop(0)
+# print(o)
 
 
-my_text = 'Hello Students'
+# # list, ordered, changeable(mutable), duplicates
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9]
 
-if 'students' in my_text.lower():
-    print('There is this word')
-else:
-    print('There is no such word')
+# # tuple   ordered, unchangeable(immutable), duplicates
+# my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 11)
 
-
-a = 10
-a = float(a)
-
-students = ('John', 'Bob', 'Alice', 'Mary')
-print(type(students))
-
-students = list(students)
-print(type(students))
-students.append('Kate')
-print(students)
-students = tuple(students)
-print(type(students))
+# if 11 in my_tuple:
+#     print("Yes")
+# else:
+#     print("No")
 
 
-start = time()
-my_list = [i for i in range(1000000)]
-end = time()
-print(end - start, 'seconds')  # 0.035776376724243164 seconds
+# my_text = 'Hello Students'
+
+# if 'students' in my_text.lower():
+#     print('There is this word')
+# else:
+#     print('There is no such word')
 
 
-start = time()
-my_tuple = (i for i in range(1000000))
-end = time()
-print(end - start, 'seconds')  # 0.00002384185791015625 seconds
+# a = 10
+# a = float(a)
+
+# students = ('John', 'Bob', 'Alice', 'Mary')
+# print(type(students))
+
+# students = list(students)
+# print(type(students))
+# students.append('Kate')
+# print(students)
+# students = tuple(students)
+# print(type(students))
 
 
-f = [1, 2, 3]
-
-g = (1, 2, 3)
-h = 1, 2, 3
-
-print(hash(h))
-print(hash(g))  # 529344067295497451
-
-a, b, c = (1, 2, (3, 4, 5))
-
-print(a, b, *c)
+# start = time()
+# my_list = [i for i in range(1000000)]
+# end = time()
+# print(end - start, 'seconds')  # 0.035776376724243164 seconds
 
 
-a = (1, 2, 3)
-print(type(a))
-
-a = (1,)
-print(type(a))
-
-b = ([1, 2, 3],)
-print(type(b))
+# start = time()
+# my_tuple = (i for i in range(1000000))
+# end = time()
+# print(end - start, 'seconds')  # 0.00002384185791015625 seconds
 
 
-a = ([1, 2, 3], [4, 5, 6], [7, 8, 9])
+# f = [1, 2, 3]
 
-a[-1].append(10)
+# g = (1, 2, 3)
+# h = 1, 2, 3
 
-print(a)
+# print(hash(h))
+# print(hash(g))  # 529344067295497451
 
+# a, b, c = (1, 2, (3, 4, 5))
 
-fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
-
-(green, yellow, *red) = fruits
-print(*red)
-
-
-fruits = ("apple", "banana", 10, "cherry", "strawberry",
-          1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, "raspberry")
-
-for i in fruits:
-    if type(i) == str:
-        print('Єдині елементи цифри це:', i)
+# print(a, b, *c)
 
 
-a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ['Roman', 'Ivan', 'Petro'])
+# a = (1, 2, 3)
+# print(type(a))
 
-for i in a:
-    try:
-        print(*i)
-    except TypeError:
-        print(i)
+# a = (1,)
+# print(type(a))
 
-
-a = ([1, 2, 3, 4], ['Roman', 'Ivan', 'Petro'])
+# b = ([1, 2, 3],)
+# print(type(b))
 
 
-for i in a:
-    if 'Ivan' in i:
-        print('This person is in the list', 'the index is', i.index('Ivan'))
+# a = ([1, 2, 3], [4, 5, 6], [7, 8, 9])
+
+# a[-1].append(10)
+
+# print(a)
 
 
-tuple1 = ("a", "b", "c")
-tuple2 = (1, 2, 3)
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+# (green, yellow, *red) = fruits
+# print(*red)
 
 
-tuple3 = tuple1 + tuple2
-print(tuple3)
+# fruits = ("apple", "banana", 10, "cherry", "strawberry",
+#           1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, "raspberry")
 
-print(tuple3 * 3)
-
-a = 'Hello'
-
-
-tuple1 = (1, 2, 3, 4, 50, 3, 2, 3, 2)
-print(tuple1.count('a'))
-print(tuple1.index('a'))
+# for i in fruits:
+#     if type(i) == str:
+#         print('Єдині елементи цифри це:', i)
 
 
-print(len(tuple1))
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ['Roman', 'Ivan', 'Petro'])
+
+# for i in a:
+#     try:
+#         print(*i)
+#     except TypeError:
+#         print(i)
+
+
+# a = ([1, 2, 3, 4], ['Roman', 'Ivan', 'Petro'])
+
+
+# for i in a:
+#     if 'Ivan' in i:
+#         print('This person is in the list', 'the index is', i.index('Ivan'))
+
+
+# tuple1 = ("a", "b", "c")
+# tuple2 = (1, 2, 3)
+
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+# print(tuple3 * 3)
+
+# a = 'Hello'
+
+
+# tuple1 = (1, 2, 3, 4, 50, 3, 2, 3, 2)
+# print(tuple1.count('a'))
+# print(tuple1.index('a'))
+
+
+# print(len(tuple1))
+
+
+# a = [('str', 10, 1.0), ('str', 10, 1.0), ('str', 10, 1.0)]
+
+
+# for i in a:
+#     i = list(i)
+#     for z in i:
+#         if type(z) == float:
+#             z = int(z)
+#         print(z, end=' ')
+
+
+# unpacking
+
+a = [1,2]
+b = [3,4]
+c = [5,6]
+
+
+fruits =[*c, *b, *c]
+
+print(fruits)
 
 
 
-a = [('str', 10, 1.0), ('str', 10, 1.0), ('str', 10, 1.0)]
 
-
-for i in a:
-    i = list(i)
-    for z in i:
-        if type(z) == float:
-            z = int(z)
-        print(z, end=' ')
-          
